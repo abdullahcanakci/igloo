f847e28b7a7cff49da4372575666333f143d7ca0
Abdullah Çanakçı <abdullahcanakci.0@gmail.com>
2019-12-23 14:39:17 +0300

    Button debouncing click+hold implemented. System timer added.

--- aurora/Software/main/app_main.c
+++ aurora/Software/main/app_main.c
@@ -1,37 +1,10 @@
 #include "app_main.h"
 
-static void gpio_isr_handler(void *args){
-    uint32_t gpio_num = (uint32_t) args;
-    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
-}
-
-static void gpio_task(void *arg){
-    uint32_t io_num;
-    for (;;){
-        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY))
-        {
-            if(io_num == GPIO_BUT_FUNC){
-                debounce(gpio_get_level(io_num));
-            }
-        }
-    }
-}
-
-static void debounce(int in_state){
-    static bool state = false; // UP
-    if (state != in_state){
-        state = in_state;
-        ESP_LOGI("Debounce", "Button %d", in_state);
-        return;
-    }
-}
-
 void app_main()
 {
-     gpio_config_t but_conf;
-    // Disable interrupts
+    // SET FUNC BUTTON
+    gpio_config_t but_conf;
     but_conf.intr_type = GPIO_INTR_ANYEDGE;
-
     but_conf.mode = GPIO_MODE_INPUT;
     but_conf.pin_bit_mask = 1ULL << GPIO_BUT_FUNC;
     but_conf.pull_up_en = 1;
@@ -43,4 +16,81 @@ void app_main()
 
     gpio_install_isr_service(0);
     gpio_isr_handler_add(GPIO_BUT_FUNC, gpio_isr_handler, (void *)GPIO_BUT_FUNC);
+
+    // SYSTEM TIMER
+    xTaskCreate(system_timer, "system_timer", 256, NULL, 11, NULL);
+}
+
+static void debounce(int in_state)
+{
+    uint32_t temp = timer;
+    if (func_button.state != in_state)
+    {
+        if (func_button.time_down - temp < 10 * 1000 / configTICK_RATE_HZ)
+        {
+            return;
+        }
+        func_button.state = in_state;
+        if (func_button.state == DOWN)
+        {
+            func_button.time_down = temp;
+        }
+        else
+        {
+            func_button.time_up = temp;
+        }
+        if (func_button.state == UP && func_button.time_down != 0)
+        {
+            uint32_t interval = (func_button.time_up - func_button.time_down) * 1000 / configTICK_RATE_HZ;
+            if (interval < 2000)
+            {
+                ESP_LOGI("BUTTON", "CLICK");
+            }
+            else
+            {
+                ESP_LOGI("BUTTON", "HOLD");
+            }
+            func_button.time_up = 0;
+            func_button.time_down = 0;
+        }
+    }
+}
+
+/******************** TASKS ************************/
+
+/**
+ * freeRTOS does not have a neat system time function.
+ * I need a basic one to debounce buttons.
+ * timer is uint32_t which can hold about 29 days in 1ms resolution.
+ */
+static void system_timer(void *arg)
+{
+    for (;;)
+    {
+        timer = timer + 1;
+        vTaskDelay(1);
+    }
+}
+
+static void gpio_task(void *arg)
+{
+    uint32_t io_num;
+    for (;;)
+    {
+        if (xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY))
+        {
+            if (io_num == GPIO_BUT_FUNC)
+            {
+                debounce(gpio_get_level(io_num));
+            }
+        }
+    }
+}
+
+/**************ISR HANDLERS***************/
+
+static void gpio_isr_handler(void *args)
+{
+    uint32_t gpio_num = (uint32_t)args;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
 }
--- aurora/Software/main/app_main.h
+++ aurora/Software/main/app_main.h
@@ -1,8 +1,6 @@
 #ifndef APP_MAIN
 #define APP_MAIN
 
-
-
 #include "driver/gpio.h"
 #include <stdint.h>
 
@@ -12,13 +10,25 @@
 #include "driver/gpio.h"
 #include "esp_log.h"
 
-
 #define GPIO_BUT_FUNC 5
 
 static xQueueHandle gpio_evt_queue = NULL;
 
+typedef struct Button {
+  enum {DOWN, UP} state;
+  uint32_t time_down;
+  uint32_t time_up;
+}Button;
+
+Button func_button = {.state = UP, .time_down = 0, .time_up = 0};
+
 static void debounce(int);
 static void gpio_isr_handler(void *args);
 static void gpio_task(void *arg);
+static void system_timer(void *arg);
+
+static uint32_t timer = 0;
+
+
 
 #endif
